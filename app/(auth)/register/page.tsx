6adba3240ff9fe8a8766d235f7226b8237ef9c30
sneha <snehamanandhar54@gmail.com>
2025-12-27 10:28:07 +0545

    register page completed

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,5 +1,13 @@
+import RegisterForm from "../_components/RegisterForm";
+
 export default function Page() {
     return (
-        <div>rEGISTER Page</div>
+        <div className="space-y-6 w-full">
+            <div className="text-center">
+                <h1 className="text-2xl text-[#E87A5D] font-semibold ">MunchNearby</h1>
+                <h2 className="text-2xl mt-6 font-semibold">Create Your Account</h2>
+            </div>
+            <RegisterForm/>
+        </div>
     );
 }
