698b6d4520010460d5fc3faa597f3803d6b46388
sneha <snehamanandhar54@gmail.com>
2026-02-11 14:53:21 +0545

    profile added in restaurant side

--- app/restaurantowner/profile/page.tsx
+++ app/restaurantowner/profile/page.tsx
@@ -1,11 +1,22 @@
+import { handleWhoAmI } from "@/lib/actions/auth-actions";
+import { notFound, redirect } from "next/navigation";
 import Sidebar from "../_components/SideBar";
+import RestaurantProfileForm from "./_components/RestaurantProfileForm";
 
-
-export default function Page() {
-    return (
-        <div className="flex">
-              <Sidebar />
-              <div className="p-6">Profile</div>
-        </div>
-    );
+export default async function Page() {
+  const result = await handleWhoAmI();
+    if(!result.success){
+        throw new Error("Error");
+    }
+    if(!result.data){
+        notFound();
+    }
+  return (
+    <div className="flex">
+      <Sidebar />
+      <div className="flex-1 p-6">
+        <RestaurantProfileForm user={result.data} />
+      </div>
+    </div>
+  );
 }
